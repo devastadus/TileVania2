194ff54d4b85de0a9f6bca554893947acb185b46
Scott Price <scottprice7@gmail.com>
2019-07-14 09:47:43 -0700

    fixing git ignore and adding

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+    [SerializeField] private float runSpeed = 5f;
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        
+    }
+
+    private void Run() {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rigidbody2D.velocity.y);
+        rigidbody2D.velocity = playerVelocity;
+    }
+}
