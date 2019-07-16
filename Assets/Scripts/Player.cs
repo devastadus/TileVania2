57e753ab4ed9cabfc2353d91d94d193eda326cf2
Scott Price <scottprice7@gmail.com>
2019-07-15 20:43:21 -0700

    fixing sorting layers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,7 @@ public class Player : MonoBehaviour {
     void Update()
     {
         Run();
+        FlipSprite();
         
     }
 
@@ -22,5 +23,12 @@ public class Player : MonoBehaviour {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rigidbody2D.velocity.y);
         rigidbody2D.velocity = playerVelocity;
+    }
+
+    private void FlipSprite() {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x),1f);
+        }
     }
 }
