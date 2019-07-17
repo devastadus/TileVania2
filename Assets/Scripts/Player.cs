af3bd2bfff36213a6dcf8e971da0b2cbde09a0fa
Scott Price <scottprice7@gmail.com>
2019-07-16 19:53:15 -0700

    up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
+    
+    //Config
     [SerializeField] private float runSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 5f;
+    [SerializeField] private float climbSpeed = 5f;
+    
+    //State
+    private bool isAlive = true;
+    
+    //Cached componet references
     private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    private Collider2D collider2D;
+    private float gravityScaleAtStart;
+    
+    //Messages then Methods
     // Start is called before the first frame update
     void Start() {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        collider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = rigidbody2D.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        ClimbLadder();
+        Jump();
         FlipSprite();
         
     }
@@ -23,6 +42,36 @@ public class Player : MonoBehaviour {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rigidbody2D.velocity.y);
         rigidbody2D.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void ClimbLadder() {
+
+        if (!collider2D.IsTouchingLayers(LayerMask.GetMask("Ladder"))) {
+            animator.SetBool("Climbing",false);
+            rigidbody2D.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        float controlFlow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(rigidbody2D.velocity.x, controlFlow * climbSpeed);
+        rigidbody2D.velocity = climbVelocity;
+        rigidbody2D.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(rigidbody2D.velocity.y) > Mathf.Epsilon;
+        animator.SetBool("Climbing",playerHasVerticalSpeed);
+    }
+
+    private void Jump() {
+        if(!collider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            return;
+        
+        if (CrossPlatformInputManager.GetButtonDown("Jump")) {
+            Vector2 jumpVelocityToAdd = new Vector2(0,jumpSpeed);
+            rigidbody2D.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite() {
